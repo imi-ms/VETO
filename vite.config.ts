01d82c91dd149cf62a53620036140ce73eea212a
gitlab-ci <ci@example.com>
2026-03-05 14:33:28 +0000

    Release v0.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/VETO/',
+  base: process.env.VITE_BASE ?? './',
   build: {
     rollupOptions: {
       output: {
